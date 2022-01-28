cd0030349719b3743e1c84cd3674ab278a748b3b
kimezawa <nkzwkk@gmail.com>
2022-01-28 10:19:26 +0900

    PHPで作成したお問合わせフォーム

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# contactform
+contactform
+====
+
+PHPで作成したお問合わせフォームです。
+
+## Demo
+
+![](demo.png)
+
+## Author
+
+[kimezawa](https://github.com/kimezawa)
--- contactform/insert.php
+++ contactform/insert.php
@@ -1,7 +1,7 @@
 <?php
 mb_internal_encoding("utf8");
 
-$pdo = new PDO("mysql:dbname=lesson01;host=localhost;","root","");
+$pdo = new PDO("mysql:dbname=lesson01;host=localhost;","root","mysql");
 
 $pdo -> exec("insert into contactform(name,mail,age,comments)
 values('".$_POST['name']."','".$_POST['mail']."','".$_POST['age']."','".$_POST['comments']."');");
